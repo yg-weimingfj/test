bd63417be415dc080e9e947bb90ed295b29bfd89
yange <yange@weimingfj.com>
2016-12-30 11:12:22 +0800

    空车上报

--- DriverIos/emptyCar/EmptyCarListViewController.swift
+++ DriverIos/emptyCar/EmptyCarListViewController.swift
@@ -15,8 +15,11 @@ class EmptyCarListViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        let xib = UINib(nibName: "EmptyCarRecordCell", bundle: nil) //nibName指的是我们创建的Cell文件名
         
+        let buttonXib  = UINib(nibName : "EmptyCarUpload",bundle: nil)
+        self.tableView.register(buttonXib,forCellReuseIdentifier: "emptyCarUpload")
+        
+        let xib = UINib(nibName: "EmptyCarRecordCell", bundle: nil) //nibName指的是我们创建的Cell文件名
         self.tableView.register(xib, forCellReuseIdentifier: "emptyCarRecordCell")
         self.tableView.tableFooterView = UIView(frame: CGRect.zero)
         self.tableView.separatorInset = UIEdgeInsets.zero
@@ -32,7 +35,7 @@ class EmptyCarListViewController: UIViewController {
         let vc = sb.instantiateViewController(withIdentifier: "emptyCarUploadRecordList") as! EmptyCarUploadRecordList
         self.present(vc, animated: true, completion: nil)
     }
-    @IBAction func UploadEmptyCar(_ sender: Any) {
+    func UploadEmptyCar() {
         let sb = UIStoryboard(name: "emptyCarUpload", bundle:nil)
         let vc = sb.instantiateViewController(withIdentifier: "emptyCarUpload") as! EmptyCarController
         self.present(vc, animated: true, completion: nil)
@@ -56,27 +59,40 @@ extension EmptyCarListViewController : UITableViewDataSource,UITableViewDelegate
         return models.count+1
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
+        if(indexPath.row == models.count){
+            return 114
+        }else{
+          return 44
+        }
+        
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "emptyCarRecordCell", for: indexPath) as! EmptyCarRecordCell
-        cell.sourceAreaLabel.text = "内蒙古 呼和浩特"
-        cell.sourceAreaLabel.sizeToFit()
-        var fromIamgeFrame = cell.fromImageView.frame
-        fromIamgeFrame.origin.x = cell.sourceAreaLabel.frame.size.width + 15
-        cell.fromImageView.frame = fromIamgeFrame
-        var destAreaFrame = cell.destAreaLabel.frame
-        destAreaFrame.origin.x = fromIamgeFrame.origin.x + fromIamgeFrame.size.width + 5
-        cell.destAreaLabel.frame = destAreaFrame
-        cell.destAreaLabel.text = "内蒙古 呼和浩特"
-        cell.destAreaLabel.sizeToFit()
-        return cell
+        if(indexPath.row == models.count){
+            let cell = tableView.dequeueReusableCell(withIdentifier:"emptyCarUpload",for: indexPath) as! EmptyCarUpload
+            cell.uploadButton.addTarget(self,action:#selector(EmptyCarListViewController.UploadEmptyCar),for:UIControlEvents.touchUpInside)
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "emptyCarRecordCell", for: indexPath) as! EmptyCarRecordCell
+            cell.sourceAreaLabel.text = "内蒙古 呼和浩特"
+            cell.sourceAreaLabel.sizeToFit()
+            var fromIamgeFrame = cell.fromImageView.frame
+            fromIamgeFrame.origin.x = cell.sourceAreaLabel.frame.size.width + 15
+            cell.fromImageView.frame = fromIamgeFrame
+            var destAreaFrame = cell.destAreaLabel.frame
+            destAreaFrame.origin.x = fromIamgeFrame.origin.x + fromIamgeFrame.size.width + 5
+            cell.destAreaLabel.frame = destAreaFrame
+            cell.destAreaLabel.text = "内蒙古 呼和浩特"
+            cell.destAreaLabel.sizeToFit()
+            return cell
+        }
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let sb = UIStoryboard(name: "emptyCarUpload", bundle:nil)
-        let vc = sb.instantiateViewController(withIdentifier: "emptyCarTableListView") as! EmptyCarTableListView
-        self.present(vc, animated: true, completion: nil)
+        if(indexPath.row != models.count){
+            tableView.deselectRow(at: indexPath, animated: true)
+            let sb = UIStoryboard(name: "emptyCarUpload", bundle:nil)
+            let vc = sb.instantiateViewController(withIdentifier: "emptyCarTableListView") as! EmptyCarTableListView
+            self.present(vc, animated: true, completion: nil)
+        }
+        
     }
 }
